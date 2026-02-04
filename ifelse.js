c7a1cf3a1e45b1e21d459f302b66bba7156a285d
Name <email>
2026-02-04 13:10:00 -0600

    first commit

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+function isSuperhero(name)
+{
+    
+    var superhero=name.charAt(0)
+if(superhero==="S" || superhero==="s")   
+{
+    console.log("The superhero is " + name);
+}else{
+    console.log("The person is not a superhero");
+}
+}
+isSuperhero("satman");
