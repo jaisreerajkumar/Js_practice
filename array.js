f9dc123b2fffa1cb4861f8ea8ea28c8cf2808248
Name <email>
2026-02-04 14:52:54 -0600

    Array file is added

---
+++ array.js
@@ -0,0 +1,19 @@
+var a=[1,2,3,4,5,6,7,8,9,10]
+console.log("The even number is ")
+for(i=0;i<=10;i++)
+{
+    
+    if(a[i]%2==0) 
+    {
+        console.log(a[i]);
+    }   
+}
+console.log("The odd number is ")
+for(i=0;i<=10;i++)
+{
+    
+    if(a[i]%2==1) 
+    {
+        console.log(a[i]);
+    }   
+}
